305004ab0704089fdbc64e5dae1355e7c481124f
Katsuyuki Omuro <komuro@fastly.com>
2022-06-09 12:22:40 +0900

    run persistence on 3001 by default

--- src/persistence/src/index.ts
+++ src/persistence/src/index.ts
@@ -4,7 +4,7 @@ import cors from 'cors';
 import { instance, AlreadyExistsError, NotFoundError } from './PersistenceApiServer';
 
 const app = express();
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT ?? 3001;
 
 app.use(cors());
 app.use(express.json());
