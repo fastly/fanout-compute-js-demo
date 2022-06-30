4d62cfb6f7de9d4e7e4df9b9fa8e123fa522ea79
Katsuyuki Omuro <komuro@fastly.com>
2022-06-30 13:19:46 +0900

    In-app links to the demo and github

--- src/client/src/pages/App/App.tsx
+++ src/client/src/pages/App/App.tsx
@@ -2,10 +2,24 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import './App.css';
 
+function Info() {
+  return (
+    <div className="Info">
+      <div>
+        <a target="_blank" href="https://developer.fastly.com/solutions/demos/realtimeqa-websockets/">Read here</a> about this demo!
+      </div>
+      <div>
+        Source code <a target="_blank" href="https://github.com/fastly/fanout-compute-js-demo">on GitHub</a>.
+      </div>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
       <Outlet />
+      <Info />
     </div>
   );
 }
